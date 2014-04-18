fa15e0972146d8f96b931fc0fb5406361f512675
Nicholas Van Wiggeren <nick@facepwn.com>
2014-04-17 20:55:12 -0700

    Update main with github url, etc

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"fmt"
 
-	ipupdate "iptodate/lib"
+	ipupdate "github.com/nickvanw/iptodate/lib"
 
 	"github.com/BurntSushi/toml"
 )
@@ -14,26 +14,15 @@ var (
 	find_id_flag       = flag.Bool("l", false, "Find the ID that corresponds to the record")
 	add_record_flag    = flag.Bool("a", false, "Add a record with the configured name")
 	update_record_flag = flag.Bool("u", false, "Update the record with the configured data and address")
-	dns_zone           = flag.String("h", "", "Host (ie: example.com) to update")
-	subdomain          = flag.String("s", "", "Subdomain (ie: home.example.com would be 'home' to update")
 )
 
 func main() {
 	flag.Parse()
-	var record_id, zone, sub string
-	var ass bool
+	var record_id string
 	var config Config
 	_, err := toml.DecodeFile(*config_flag, &config)
 	if err != nil {
 		panic(err)
-	}
-	zone, ass = AssignVar(*dns_zone, config.Domain)
-	if !ass {
-		fmt.Println("I need a DNS zone in the config file or as a flag!")
-	}
-	sub, ass = AssignVar(*subdomain, config.Name)
-	if !ass {
-		fmt.Println("I need a subdomain, either in the config file or as a flag!")
 	}
 	if !*add_record_flag && !*find_id_flag && !*update_record_flag {
 		fmt.Println("I didn't get any arguments! Run with the -h command to see what they are")
@@ -44,7 +33,18 @@ func main() {
 			fmt.Println("There's already a record with that name!, run this program without the -a flag to change it")
 			return
 		}
-		fmt.Println(err)
+		ip, err := ipupdate.FetchIP(config.IPAPI)
+		if err != nil {
+			fmt.Println("Error fetching IP:", err)
+			return
+		}
+		id, err := ipupdate.CreateRecord(ip, config.Domain, config.Name, config.APIEmail, config.APIKey)
+		if err != nil {
+			fmt.Println("Error creating record: ", err)
+			return
+		}
+		fmt.Println("Created record with ID:", id)
+		record_id = id
 	}
 	if *find_id_flag {
 		id, err := ipupdate.GetId(config.Domain, config.APIEmail, config.APIKey, config.Name)
@@ -71,19 +71,6 @@ func main() {
 		}
 		fmt.Println("IP Updated successfully to", ip)
 	}
-}
-
-func AssignVar(first string, second string) (string, bool) {
-	if first == "" {
-		if second == "" {
-			return "", false
-		}
-		return first, true
-	}
-	if second == "" {
-		return "", false
-	}
-	return second, true
 }
 
 type Config struct {
